550cca19f5c24967911ba728b4a7a03781e9273a
ElenaVedernikova <lenok-v@live.ru>
2021-07-20 14:42:45 +0300

    correct test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("{name: Dorothy Zborna}"))
+                .body("data", equalTo("{name: Dorothy Zborna, type: Individual}"))
                 .body("headers", hasKey("user-agent"))
                 .header("Content-Type", "application/json; charset=utf-8")
                 .contentType(ContentType.JSON)
